6a453eb941507098aa11ab78598616a401ab6921
DianaTsebenko <cebenkodiana@gmail.com>
2024-03-24 21:41:44 +0200

    Fix read more buttons

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const readMoreButton = document.querySelector(".program_read_more");
-  const hiddenText = document.getElementById("hidden-text");
+  const readMoreButtons = document.querySelectorAll(".read_more");
+  readMoreButtons.forEach(function (readMoreButton) {
+    const hiddenText = document.getElementById(readMoreButton.getAttribute("for"));
 
-  readMoreButton.addEventListener("click", function() {
-    if (hiddenText.classList.contains("visible-text")) {
-      hiddenText.classList.remove("visible-text");
-      hiddenText.classList.add("hidden-text");
-      readMoreButton.querySelector("span").textContent = "Read more";
-    } else {
-      hiddenText.classList.remove("hidden-text");
-      hiddenText.classList.add("visible-text");
-      readMoreButton.querySelector("span").textContent = "Less";
-    }
+    readMoreButton.addEventListener("click", function () {
+      if (hiddenText.classList.contains("visible-text")) {
+        hiddenText.classList.remove("visible-text");
+        hiddenText.classList.add("hidden-text");
+        readMoreButton.querySelector("span").textContent = "Read more";
+      } else {
+        hiddenText.classList.remove("hidden-text");
+        hiddenText.classList.add("visible-text");
+        readMoreButton.querySelector("span").textContent = "Less";
+      }
+    });
   });
  
   const openMenuBtn = document.querySelector("[data-menu-open]");
